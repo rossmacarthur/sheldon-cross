31102e8bf251220d3c59fc4497a7bd3c75e6b1df
Ross MacArthur <ross@macarthur.io>
2022-10-16 13:46:17 +0200

    Rewrite tool

--- render.rs
+++ render.rs
@@ -2,11 +2,7 @@ use std::fs;
 use std::path::PathBuf;
 
 use anyhow::Context;
-use clap::Parser;
-use handlebars::Handlebars;
-use serde_json::json;
-
-const TEMPLATE: &str = include_str!("docker/Dockerfile");
+use argh::FromArgs;
 
 /// Renders a Dockerfile at `docker/Docker.<target>`.
 ///
@@ -14,37 +10,33 @@ const TEMPLATE: &str = include_str!("docker/Dockerfile");
 /// images for use with [`sheldon`]'s CI.
 ///
 /// [`sheldon`]: https://github.com/rossmacarthur/sheldon
-#[derive(Debug, Parser)]
+#[derive(Debug, FromArgs)]
 struct Opts {
-    /// The target triple.
-    #[clap(long)]
+    /// the target triple
+    #[argh(option)]
     target: String,
-    /// OpenSSL install args.
-    #[clap(long)]
+    /// extra OpenSSL install args
+    #[argh(option)]
     install_openssl_args: String,
 }
 
 fn main() -> anyhow::Result<()> {
-    let opts = Opts::parse();
-    let mut handlebars = Handlebars::new();
-    handlebars.set_strict_mode(true);
-    handlebars
-        .register_template_string("dockerfile", TEMPLATE)
-        .context("failed to register template")?;
-    let path: PathBuf = ["docker", &format!("Dockerfile.{}", opts.target)]
-        .iter()
-        .collect();
-    let data = json!({
-        "target": opts.target,
-        "is_musl": opts.target.contains("musl"),
-        "install_openssl_args": opts.install_openssl_args,
-    });
-    let rendered = handlebars
-        .render("dockerfile", &data)
-        .context("failed to render Dockerfile")?;
-    fs::write(&path, &rendered)
+    let opts: Opts = argh::from_env();
+
+    let result = upon::Engine::new()
+        .compile(include_str!("docker/Dockerfile"))
+        .context("failed to compile template")?
+        .render(upon::value! {
+            target: &opts.target,
+            is_musl: opts.target.contains("musl"),
+            install_openssl_args: opts.install_openssl_args,
+        })
+        .context("failed to render template")?;
+
+    let path = PathBuf::from_iter(["docker", &format!("Dockerfile.{}", opts.target)]);
+    fs::write(&path, &result)
         .with_context(|| format!("failed to write to `{}`", path.display()))?;
-    eprintln!("Rendered `{}`\n", path.display());
-    println!("{}", rendered);
+    eprintln!("Rendered {}\n", path.display());
+    println!("{}", result);
     Ok(())
 }
